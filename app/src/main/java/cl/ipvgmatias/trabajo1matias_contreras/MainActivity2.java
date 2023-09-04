855c8d3494af90c779d75d4b48edb7e381b4d58c
Alumnos <matias.contreras4@virginiogomez.cl>
2023-09-04 12:22:30 -0300

    soy el mjor

---
+++ app/src/main/java/cl/ipvgmatias/trabajo1matias_contreras/MainActivity2.java
@@ -0,0 +1,14 @@
+package cl.ipvgmatias.trabajo1matias_contreras;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
